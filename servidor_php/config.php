a8e9b7a303624d6c1809f36e2fa602882052c419
mvictor10 <mvictordossantos.limite@gmail.com>
2023-09-27 01:20:43 -0300

    update  app

--- servidor_php/config.php
+++ servidor_php/config.php
@@ -1,7 +1,2 @@
 <?php 
-	//define('HOST', 'localhost');
-	//define('USER', 'mvictor');
-	//define('PASS', '65564747');
-	//define('BASE', 'image_db');
-	$conn = new MySQLi("localhost", "mvictor", "65564747", "image_db");
-	//$conn = new MySQLi(HOST, USER, PASS, BASE);
+	$conn = new MySQLi("<host>", "<your_user>", "<your_password>", "image_db");
