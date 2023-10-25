df408bad3d268970575d850949e922943c6ece74
manoelvitorsilvasantos <manoelvitorsilvasantos@gmail.com>
2023-10-24 21:31:14 -0300

    Update fix!

--- arduino_source/main.ino
+++ arduino_source/main.ino
@@ -1,9 +1,6 @@
 //
-#include <Servo.h>
-
 const int LED_RED = 9;
 const int LED_GREEN = 8;
-Servo meuServo;
 int dt = 0;
 
 void setup()
@@ -11,7 +8,6 @@ void setup()
   Serial.begin(9600);
   pinMode(LED_RED, OUTPUT);
   pinMode(LED_GREEN, OUTPUT);
-  meuServo.attach(9);
   digitalWrite(LED_GREEN, LOW);
   digitalWrite(LED_RED, LOW);
 }
@@ -20,13 +16,11 @@ void loop()
 {
   dt = Serial.parseInt();
   if(dt == 1){
-  	meuServo.write(90);
   	digitalWrite(LED_GREEN, HIGH);
   	digitalWrite(LED_RED, LOW);
-    delay(4000);
+    delay(3500);
   }
   else{
-  	meuServo.write(0);
   	digitalWrite(LED_GREEN,LOW);
   	digitalWrite(LED_RED, HIGH);
   	delay(1000);
